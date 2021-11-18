9f13177d1df70280e91426f9aadaa7b61338b2aa
Tejinder Singh <dev.singhtejinder@gmail.com>
2021-11-18 14:17:20 -0500

    configuration: webpack for img files

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,9 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      }
+      },{
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000' }
     ]
   },
   plugins: [
