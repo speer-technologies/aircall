cecc2708c698d6cc83b8f3fd46b725b8a8529d40
Ansaar Ahmed <ansaar.ahmed245@gmail.com>
2022-02-05 00:33:51 -0500

    styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
+import './css/activity.css';
+import './css/footer.css';
+import './css/calldetail.css';
 
 import App from './App.jsx';
-
