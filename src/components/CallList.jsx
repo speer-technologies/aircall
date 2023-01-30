0e751ee96ea30d7454c25a2bc5bd0c298ea7a1a1
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-30 17:43:26 -0500

    fetch allCalls api and log it

--- src/components/CallList.jsx
+++ src/components/CallList.jsx
@@ -1,15 +1,28 @@
 //External Modules
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 //Internal Modules
 import CallListItem from './CallListItem.jsx';
 
 const CallList = () => {
-  return (
-    <div>
-      <CallListItem />
-    </div>
-  );
+  const [calls, setCalls] = useState([]);
+
+  // Fetch all calls
+  useEffect(() => {
+    axios
+      .get(
+        'https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities'
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(`The Error is: ${error}`);
+      });
+  }, []);
+
+  return <div></div>;
 };
 
 export default CallList;
