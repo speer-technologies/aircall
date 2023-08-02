244d2a79ce93247336180adcdc1421c62a941d1b
javascriptsucks <evix1988@live.cn>
2023-08-02 14:44:14 -0600

    add MaterialUI, Redux toolkit, also update react 16 into 18

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-import Header from './Header.jsx';
+import Header from "./Header.jsx";
 
 const App = () => {
-  return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
-    </div>
-  );
+	return (
+		<React.StrictMode>
+			<Provider store={store}>
+				<div className='container'>
+					<Header />
+					<div className='container-view'>Some activities should be here</div>
+				</div>
+			</Provider>
+		</React.StrictMode>
+	);
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import './css/body.css';
-import './css/app.css';
-import './css/header.css';
+import "./css/body.css";
+import "./css/app.css";
+import "./css/header.css";
 
-import App from './App.jsx';
-
+import App from "./App.jsx";
