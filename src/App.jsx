a318f4c0f1406ae022b8e91501aa2f205ec33e23
bensmaj <ben.stambuk@gmail.com>
2024-07-18 12:23:52 -0400

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Header from './Header.jsx';
+import Header from "./Header.jsx";
 
 const App = () => {
   return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
+    <div className="container">
+      <Header />
+      <div className="container-view text-xl">
+        Some activities should be here
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './css/body.css';
-import './css/app.css';
-import './css/header.css';
+import "./css/body.css";
+import "./css/app.css";
+import "./css/header.css";
+import "./output.css";
 
-import App from './App.jsx';
-
+import App from "./App.jsx";
