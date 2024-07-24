ba315722ac390402641ac7d398046c3fab99dd12
HVMS <viralsid2330@gmail.com>
2024-07-24 17:21:19 -0300

    fix: aircall assignment

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './Header.jsx';
+import ActivityFeed from './components/activity-feed/ActivityFeed.jsx';
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
-      <div className="container-view">Some activities should be here</div>
+      <div className="container-view">
+        <ActivityFeed />
+      </div>
     </div>
   );
 };
